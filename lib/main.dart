a582d9c1270e65a32822de48ce5ad3f8c9789a47
IbrokhimSidikov <ibrokhim.sidikov@gmail.com>
2025-08-14 19:57:29 +0500

    a

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
           textTheme: const TextTheme(
             bodyMedium: TextStyle(
               fontSize: 20,
+              fontWeight: FontWeight.w600,
             ),
           ),
         ),
